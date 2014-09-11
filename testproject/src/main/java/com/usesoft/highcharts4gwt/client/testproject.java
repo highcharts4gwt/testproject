e100dc243eba2a3e2b9edfc88b3199838d4a2bfa
Ronan Quillevere <ronan.quillevere@gmail.com>
2014-09-11 19:04:12 +0200

    try to make JsoArray work

--- testproject/src/main/java/com/usesoft/highcharts4gwt/client/testproject.java
+++ testproject/src/main/java/com/usesoft/highcharts4gwt/client/testproject.java
@@ -9,12 +9,13 @@ import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.usesoft.highcharts4gwt.client.view.widget.HighchartsLayoutPanel;
-import com.usesoft.highcharts4gwt.model.array.api.ArrayNumber;
+import com.usesoft.highcharts4gwt.model.array.api.Array;
 import com.usesoft.highcharts4gwt.model.array.api.ArrayString;
 import com.usesoft.highcharts4gwt.model.array.jso.JsoArrayNumber;
 import com.usesoft.highcharts4gwt.model.array.jso.JsoArrayString;
-import com.usesoft.highcharts4gwt.model.highcharts.api.ChartOptions;
+import com.usesoft.highcharts4gwt.model.highcharts.api.Series;
 import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoChartOptions;
+import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoSeries;
 
 public class testproject implements EntryPoint
 {
@@ -35,7 +36,7 @@ public class testproject implements EntryPoint
 
         // OptionsFactory factory = new OptionsFactoryJso();
 
-        ChartOptions options = (JsoChartOptions) JavaScriptObject.createObject();
+        JsoChartOptions options = (JsoChartOptions) JavaScriptObject.createObject();
 
         options.subtitle().text("sub ronan");
         options.title().text("Title");
@@ -50,16 +51,19 @@ public class testproject implements EntryPoint
         // Options options = factory.create();
         // options.title().text("Test Ronan 2");
 
-        // Series value = (JsoSeries) JavaScriptObject.createObject();
-        ArrayNumber data = (JsoArrayNumber) JsArrayNumber.createArray();
+        JsoSeries value = (JsoSeries) JavaScriptObject.createObject();
+        JsoArrayNumber data = (JsoArrayNumber) JsArrayNumber.createArray();
         data.insert(0, 10);
         data.insert(0, 20);
         data.insert(0, 30);
         data.insert(0, 40);
         data.insert(0, 50);
-        // value.data(data);
+        value.data(data);
 
-        // options.series().push(value);
+        Array<Series> series = options.series();
+
+        series.addToEnd(value);
+
         container.renderChart(options);
 
         // Timer timer = new Timer() {
