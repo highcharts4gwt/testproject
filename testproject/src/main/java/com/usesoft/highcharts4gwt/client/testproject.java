f017c1b399d50c0bb3ba47d2a9f968866e1b1e07
Ronan Quillevere <ronan.quillevere@gmail.com>
2014-09-23 20:56:12 +0200

    First display of a graph !!!!

--- testproject/src/main/java/com/usesoft/highcharts4gwt/client/testproject.java
+++ testproject/src/main/java/com/usesoft/highcharts4gwt/client/testproject.java
@@ -2,6 +2,7 @@ package com.usesoft.highcharts4gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JsArrayNumber;
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.dom.client.Style.Unit;
@@ -10,12 +11,22 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.usesoft.highcharts4gwt.client.view.widget.HighchartsLayoutPanel;
 import com.usesoft.highcharts4gwt.model.array.api.Array;
+import com.usesoft.highcharts4gwt.model.array.api.ArrayNumber;
 import com.usesoft.highcharts4gwt.model.array.api.ArrayString;
 import com.usesoft.highcharts4gwt.model.array.jso.JsoArrayNumber;
 import com.usesoft.highcharts4gwt.model.array.jso.JsoArrayString;
+import com.usesoft.highcharts4gwt.model.highcharts.api.ChartOptions;
+import com.usesoft.highcharts4gwt.model.highcharts.api.Lang;
 import com.usesoft.highcharts4gwt.model.highcharts.api.Series;
+import com.usesoft.highcharts4gwt.model.highcharts.api.Subtitle;
+import com.usesoft.highcharts4gwt.model.highcharts.api.Title;
+import com.usesoft.highcharts4gwt.model.highcharts.api.XAxis;
 import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoChartOptions;
+import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoLang;
 import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoSeries;
+import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoSubtitle;
+import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoTitle;
+import com.usesoft.highcharts4gwt.model.highcharts.jso.JsoXAxis;
 
 public class testproject implements EntryPoint
 {
@@ -36,34 +47,75 @@ public class testproject implements EntryPoint
 
         // OptionsFactory factory = new OptionsFactoryJso();
 
-        JsoChartOptions options = (JsoChartOptions) JavaScriptObject.createObject();
+        ChartOptions options = (JsoChartOptions) JavaScriptObject.createObject();
+        
+        Subtitle subTitle = (JsoSubtitle) JavaScriptObject.createObject();
+        subTitle.text("Chart reflow is set to true");
 
-        options.subtitle().text("sub ronan");
-        options.title().text("Title");
+        Title title = (JsoTitle) JavaScriptObject.createObject();
+        title.text("When resizing the window or the frame, the chart should resize");
+        
+        options.subtitle(subTitle);
+        options.title(title);
 
         ArrayString months = (JsoArrayString) JsArrayString.createArray();
         months.push("janvier");
         months.push("fev");
         months.push("mars");
         months.push("avril");
-        options.lang().months(months);
+        
+        Lang lang = (JsoLang) JavaScriptObject.createObject();
+        lang.months(months);
+        options.lang(lang);
 
         // Options options = factory.create();
         // options.title().text("Test Ronan 2");
 
-        JsoSeries value = (JsoSeries) JavaScriptObject.createObject();
-        JsoArrayNumber data = (JsoArrayNumber) JsArrayNumber.createArray();
-        data.insert(0, 10);
-        data.insert(0, 20);
-        data.insert(0, 30);
-        data.insert(0, 40);
-        data.insert(0, 50);
+        Series value = (JsoSeries) JavaScriptObject.createObject();
+        
+        ArrayNumber data = (JsoArrayNumber) JsArrayNumber.createArray();
+        
+        data.push(29.9);
+        data.push(71.5);
+        data.push(106.4);
+        data.push(129.2);
+        data.push(144.0);
+        data.push(176.0);
+        data.push(135.6);
+        data.push(148.5);
+        data.push(216.4);
+        data.push(194.1);
+        data.push(95.6);
+        data.push(54.4);
+        
+        
         value.data(data);
 
-        Array<Series> series = options.series();
-
+        Array<Series> series = (Array<Series>) JsArray.createArray();
+        
         series.addToEnd(value);
-
+        
+        XAxis xAxis = (JsoXAxis) JavaScriptObject.createObject();
+        
+        ArrayString categories = (JsoArrayString) JsArrayString.createArray();
+        categories.push("Jan");
+        categories.push("Feb");
+        categories.push("Mar");
+        categories.push("Apr");
+        categories.push("May");
+        categories.push("Jun");
+        categories.push("Jul");
+        categories.push("Aug");
+        categories.push("Sep");
+        categories.push("Oct");
+        categories.push("Nov");
+        categories.push("Dec");
+        
+		xAxis.categories(categories);
+		
+		options.xAxis(xAxis);
+		options.series(series);
+		
         container.renderChart(options);
 
         // Timer timer = new Timer() {
