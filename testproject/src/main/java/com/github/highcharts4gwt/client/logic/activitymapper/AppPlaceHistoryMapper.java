a8cf7f571ba66889a73c5235f0032336d299714f
Ronan Quillevere <ronan.quillevere@gmail.com>
2015-01-06 13:10:36 +0100

    Use new snapshot version

--- testproject/src/main/java/com/github/highcharts4gwt/client/logic/activitymapper/AppPlaceHistoryMapper.java
+++ testproject/src/main/java/com/github/highcharts4gwt/client/logic/activitymapper/AppPlaceHistoryMapper.java
@@ -5,7 +5,7 @@ import com.github.highcharts4gwt.client.logic.place.HighMapsPlace;
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
 
-@WithTokenizers({ ChartPlace.Tokenizer.class, HighMapsPlace.Tokenizer.class })
+@WithTokenizers({ChartPlace.Tokenizer.class, HighMapsPlace.Tokenizer.class })
 public interface AppPlaceHistoryMapper extends PlaceHistoryMapper
 {
 }
